45d889db32caa20185a0e55dbfa614ce2b1760a0
codingcodey <76607086+codingcodey@users.noreply.github.com>
2021-03-08 21:59:57 +1100

    Update images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -983,6 +983,150 @@ c 1 f 1 c
 . e 5 c . 
 . . c . . 
 `;
+            case "image116":return img`
+. . 5 . . 
+. 8 4 2 . 
+f 4 5 4 f 
+. 2 4 8 . 
+. . 5 . . 
+`;
+            case "image117":return img`
+. . 5 . . 
+. 2 2 2 . 
+5 2 4 2 5 
+. 2 2 2 . 
+. . 5 . . 
+`;
+            case "image118":return img`
+. . . . c c c c c c . . . . . . 
+. . . c 6 7 7 7 7 6 c . . . . . 
+. . c 7 7 7 7 7 7 7 7 c . . . . 
+. c 6 7 7 7 7 7 7 7 7 6 c . . . 
+. c 7 c 6 6 6 6 c 7 7 7 c . . . 
+. f 7 6 f 6 6 f 6 7 7 7 f . . . 
+. f 7 7 7 7 7 7 7 7 7 7 f . . . 
+. . f 7 7 7 7 6 c 7 7 6 f c . . 
+. . . f c c c c 7 7 6 f 7 7 c . 
+. . c 7 2 7 7 7 6 c f 7 7 7 7 c 
+. c 7 7 2 7 7 c f c 6 7 7 6 c c 
+c 1 1 1 1 7 6 f c c 6 6 6 c . . 
+f 1 1 1 1 1 6 6 c 6 6 6 6 f . . 
+f 6 1 1 1 1 1 6 6 6 6 6 c f . . 
+. f 6 1 1 1 1 1 1 6 6 6 f . . . 
+. . c c c c c c c c c f . . . . 
+`;
+            case "image119":return img`
+. . 5 . . 
+. 8 4 2 . 
+f 4 5 4 f 
+. 2 4 8 . 
+. . 5 . . 
+`;
+            case "image120":return img`
+. . 
+. . 
+. . 
+5 5 
+5 5 
+5 5 
+5 5 
+5 5 
+`;
+            case "image121":return img`
+. . f . . 
+. 2 f 5 . 
+f f f f f 
+. 5 f 2 . 
+. . f . . 
+`;
+            case "image122":return img`
+. . . . . . . c d . . . . . . . 
+. . . . . . . c d . . . . . . . 
+. . . . . . . c d . . . . . . . 
+. . . . . . . c b . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . c 2 . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . e 2 . . . . . . . 
+. . . . . . e e 4 e . . . . . . 
+. . . . . . e 2 4 e . . . . . . 
+. . . . . c c c e e e . . . . . 
+. . . . e e 2 2 2 4 e e . . . . 
+. . c f f f c c e e f f e e . . 
+. c c c c e e 2 2 2 2 4 2 e e . 
+c c c c c c e e 2 2 2 4 2 2 e e 
+c c c c c c e e 2 2 2 2 4 2 e e 
+`;
+            case "image123":return img`
+. . 9 . . 
+. 9 4 9 . 
+9 4 5 4 9 
+. 9 4 9 . 
+. . 9 . . 
+`;
+            case "image124":return img`
+. . 4 . . 
+. 3 2 3 . 
+4 8 8 8 4 
+. 3 2 3 . 
+. . 4 . . 
+`;
+            case "image125":return img`
+. . 5 . . 
+. e 5 e . 
+5 5 2 5 5 
+. e 5 e . 
+. . 5 . . 
+`;
+            case "image126":return img`
+. . 8 . . 
+. 5 2 5 . 
+8 4 8 4 8 
+. 5 2 5 . 
+. . 8 . . 
+`;
+            case "image127":return img`
+. . 3 . . 
+. 3 5 3 . 
+3 8 3 8 3 
+. 3 5 3 . 
+. . 3 . . 
+`;
+            case "image128":return img`
+. . 2 . . 
+. 4 4 4 . 
+5 4 8 4 5 
+. 4 4 4 . 
+. . 2 . . 
+`;
+            case "image129":return img`
+. . a . . 
+. a a a . 
+a a 2 a a 
+. a a a . 
+. . a . . 
+`;
+            case "image130":return img`
+. . 9 . . 
+. e a e . 
+9 3 4 3 9 
+. e a e . 
+. . 9 . . 
+`;
+            case "image131":return img`
+. . c . . 
+. a 4 2 . 
+9 e 5 1 d 
+. 2 3 a . 
+. . f . . 
+`;
+            case "image132":return img`
+. . c . . 
+. c 5 e . 
+c 1 f 1 c 
+. e 5 c . 
+. . c . . 
+`;
         }
         return null;
     })
